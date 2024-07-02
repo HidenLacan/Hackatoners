4836e91b0806dcd017eff56e3805b6df2497a14f
Luis <luisernestoperezbello@gmail.com>
2024-07-02 08:30:52 -0600

    Version With Template

---
+++ retail_app/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views 
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.principal_view,name="home"),
+]
+
